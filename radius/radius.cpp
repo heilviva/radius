a7abea4ba3a4f971f418b29b80163be037283e26
heilviva <ep77777777@yandex.ru>
2024-02-06 21:57:25 +0300

    Добавьте файлы проекта.

---
+++ radius/radius.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+float areaOfCircle(float radius) {
+    return 3.14 * radius * radius;
+}
+
+float areaOfRing(float innerRadius, float outerRadius) {
+    float areaInnerCircle = areaOfCircle(innerRadius);
+    float areaOuterCircle = areaOfCircle(outerRadius);
+    float areaRing = areaOuterCircle - areaInnerCircle;
+    return areaRing;
+}
+
+int main() {
+    
+    float innerRadius, outerRadius;
+
+    cout << "Введите внутренний радиус кольца: ";
+    setlocale(LC_ALL, "Russ");
+    cin >> innerRadius;
+
+    cout << "Введите внешний радиус кольца: ";
+    cin >> outerRadius;
+
+    if (innerRadius >= outerRadius) {
+        cout << "Ошибка. Внутренний радиус должен быть меньше внешнего радиуса." << endl;
+        return 1;
+    }
+
+    float ringArea = areaOfRing(innerRadius, outerRadius);
+    cout << "Площадь кольца: " << ringArea << endl;
+
+    return 0;
+}
+
+
